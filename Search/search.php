4bd3ad3e327f1fbc4a88dfc9a20e813758f4c8e8
Manjot Sidhu <manjot.gni@gmail.com>
2017-05-23 11:12:52 +0530

    Now clicking users in search box redirects to view profile

--- Search/search.php
+++ Search/search.php
@@ -14,9 +14,12 @@ $final_username = str_ireplace($q, $b_username, $s_username);
 $final_email = str_ireplace($q, $b_email, $s_email);
 $s_gender = $row['Gender'];
 $s_img = $row['image'];
+$s_id= $row['user_id'];
 ?>
 <div class="show showss" align="left">
+<a href="../cg_view_profile/view_profile.php?id=<?php echo $s_id; ?>" style="text-decoration:none">
 <img class= "img rounded" src="../../cg_users/<?php echo $s_gender; ?>/<?php echo $s_email; ?>/Profile/<?php echo $s_img; ?>" style="width:50px; height:50px; float:left; margin-right:6px;" /><span class="name"><?php echo ucfirst($final_username); ?></span>&nbsp;<br/><?php echo $final_email; ?><br/><br/>
+</a>
 </div>
 <?php
 }
