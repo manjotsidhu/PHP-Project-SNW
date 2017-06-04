7fea2a396e6a4bafb7749918c32fbd24b1b8f26b
Manjot Sidhu <manjot.gni@gmail.com>
2017-06-04 11:57:18 +0530

    Force Desktop View on mobile devices(Issue #1)

--- Forgot_Password2.php
+++ Forgot_Password2.php
@@ -12,7 +12,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="viewport" content="width=1024,initial-scale=0">
 <link href="Main_Template/js/tether.min.js?<?php echo time(); ?>" rel="stylesheet">
 <link href="Bootstrap_4/css/bootstrap.min.css?<?php echo time(); ?>" rel="stylesheet">
 </head>
